5dbfe17377ead485f061f22d8bb4a90583e7c667
Elias Habash <eliashabash23@yahoo.com>
2019-03-26 23:25:15 -0700

    Updated skills

--- src/components/MySkillsBar.js
+++ src/components/MySkillsBar.js
@@ -9,12 +9,13 @@ class MySkillsBar extends Component {
       {type: "Java", level: 80},
       {type: "HTML/HTML5", level: 75},
       {type: "CSS/CSS3", level: 75},
-      {type: "Javascript/Typescript", level: 75},
+      {type: "Javascript", level: 75},
       {type: "jQuery", level: 60},
       {type: "React.js", level: 60},
       {type: "Node.js", level: 60},
       {type: "MongoDB", level: 60},
       {type: "Angular2", level: 50},
+      {type: "TypeScript", level: 50},
       {type: "Python", level: 50},
       {type: "MySQL", level: 50}
     ];
