3b7c83de0a3f65eed9485236114e069a831d94c9
Elias Habash <eliashabash23@yahoo.com>
2020-09-04 15:44:53 -0700

    Adding skills back

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Projects from "./Projects.js";
 import ProfilePic from "../screenshots/ProfilePic.jpg";
 import Footer from "./Footer";
+import MySkillsBar from "./MySkillsBar.js";
 
 export default class Landing extends Component {
   render() {
@@ -12,22 +13,20 @@ export default class Landing extends Component {
             <div className="col-lg-8">
               <h2 id="Top" className="about-me-header">Welcome to My Web Portfolio!</h2>
               <p className="about-me">
-                Hello, my name is Elias Habash and I am a recent graduate
-                from San Jose State University. I graduated with a Bachelor’s of
-                Science in Software Engineering in December of 2018 and I'm
-                currently actively looking for entry-level/Jr. software
-                engineering/developer roles.
+                Full stack engineer based in San Jose California. My main expertise 
+                is in web development and the tools I use today are MERN stack and 
+                AWS for development. 
               </p>
 
               <p className="about-me">
-                My main expertise is in web development using the MERN Stack and
-                mobile development for Android. My favorite langauge to code in
-                is Java but am confident in my ability to efficiently learn any 
-                programming language or framework.
+                I don’t consider myself an expert in any 
+                particular language or framework. Im an engineer at heart, 
+                and I like to solve problems. I take pride in my ability to to 
+                tackle any project or problem in any language and grind out a solution.
               </p>
 
               <p className="about-me">
-                Check out my web and mobile projects below! If you're a
+                Check out my projects below! If you're a
                 recruiter looking to connect, feel free to email me at
                 eliashabash18@gmail.com or call my cell phone number at
                 (408) 658-4036 to get in touch.
@@ -41,6 +40,7 @@ export default class Landing extends Component {
               />
             </div>
           </div>
+          <MySkillsBar />
           <hr />
           <Projects />
         </div>
--- src/components/MySkillsBar.js
+++ src/components/MySkillsBar.js
@@ -6,20 +6,15 @@ class MySkillsBar extends Component {
   constructor(props) {
     super(props);
     this.skills = [
-      {type: "Java", level: 80},
-      {type: "Android", level: 75},
       {type: "HTML/HTML5", level: 75},
       {type: "CSS/CSS3", level: 75},
       {type: "JavaScript", level: 75},
-      {type: "Bootstrap", level: 60},
       {type: "jQuery", level: 60},
       {type: "React.js", level: 60},
       {type: "Node.js", level: 60},
       {type: "MongoDB", level: 60},
-      {type: "Angular2", level: 50},
-      {type: "TypeScript", level: 50},
-      {type: "Python", level: 50},
-      {type: "MySQL", level: 50}
+      {type: "Java", level: 60},
+      {type: "AWS", level: 50}
     ];
     this.colors = {
       "bar": "#363c47",
