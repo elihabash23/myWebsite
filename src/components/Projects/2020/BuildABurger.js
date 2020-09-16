5c4c8cbda55518fab58f190bb400f4b94c5d5b41
Elias Habash <eliashabash23@yahoo.com>
2020-09-16 03:02:30 -0700

    Adding BuildABurger to portfolio

---
+++ src/components/Projects/2020/BuildABurger.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Carousel } from 'react-responsive-carousel';
+import imageNotFound from '../../../screenshots/imageNotFound.png';
+
+class BuildABurger extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <Carousel >
+							<div>
+                  		<img style={{border: '1px solid #dee2e6', borderRadius: '.25rem'}} src={imageNotFound} alt="BuildABurger img 1"/>
+                  	</div>
+					</Carousel>
+            </div>
+
+            <div className="col-lg-6">
+              <div className="project-item">
+                <h2 style={{display: "inline-block", paddingRight: "10px"}}>BuildABurger</h2> 
+                    <a class="github-button" 
+                      href="https://github.com/elihabash23/Build-A-Burger" 
+                      data-icon="octicon-star" 
+                      aria-label="Star ntkme/github-buttons on GitHub">Star</a>
+                <h4> Full Stack Developer </h4>
+                <div className="project-description">
+                  <p>
+										A React web application where users can build their own burger. 
+										Front end technologies consisted of the JavaScript, HTML, CSS, React.js, and Redux. 
+                  </p>
+                </div>
+                <div className="project-technologies">
+                  <ul className="skills">
+							<li>React</li>
+                  	<li>Redux</li>
+                  	<li>HTML</li>
+                  	<li>CSS</li>
+                  	<li>JavaScript</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default BuildABurger;
